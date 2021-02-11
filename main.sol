1cda1b3b89cba28733d58f756ebc7daae1b99aae
niv <naoryniv@gmail.com>
2021-02-11 20:30:26 +0200

    second commit

--- main.sol
+++ main.sol
@@ -1,8 +1,37 @@
 
-pragma solidity >=0.7.0 <0.8.0 
+pragma solidity >=0.7.0 <0.8.0 ;
 
-conrtact User{
+contract NBA_gembller{
 
-
-
+struct User{
+ string name;
+ string username;//address
+ string password;
+ uint level; 
 }
+
+struct BPlayer{
+  string name;
+  uint rebounds;
+  uint asists;
+  uint pointes;
+  uint blocks;
+  uint steals;
+  uint rank;
+}
+
+
+struct UserGamble{
+    string username;
+    BPlayer [5] players;
+    uint gambleSum; 
+}
+
+struct GamblingTeam{
+ UserGamble [] userGambles;
+ //DATE;
+ uint daysPassed; 
+   }
+
+   
+ }
