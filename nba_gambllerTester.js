c89d6b474d8f0dfe5c894f0345477bf209699a1b
Asaf Suryano <asafsuryano@gmail.com>
2021-03-04 19:05:38 +0200

    contracts removed

--- nba_gambllerTester.js
+++ nba_gambllerTester.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const compiledContractInterface=require('./bin/contracts/NBA_gembller.json');
+const compiledContractInterface=require('./bin/NBA_gembller.json');
 const interface=compiledContractInterface.abi;
 const bytecode=compiledContractInterface.bytecode;
 var accounts;
